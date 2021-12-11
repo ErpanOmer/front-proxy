cd625ea11dc085355c1c487e132f7a89e1a68bf6
ErpanOmer <354073823@qq.com>
2021-12-12 00:49:18 +0800

    chore: 隐藏access key

--- config/deploy.js
+++ config/deploy.js
@@ -28,8 +28,8 @@ async function upload () {
     // yourRegion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
     region: 'oss-cn-shenzhen',
     // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-    accessKeyId: 'LpNwDlH09Y5w4Evr',
-    accessKeySecret: '6SmUFd506zfCs2exivNlQwtkx3qOTN',
+    accessKeyId: '',
+    accessKeySecret: '',
     bucket: 'balqish-oss-service',
   })
 
